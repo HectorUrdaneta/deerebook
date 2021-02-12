e505fa1481fe3d459cb9e73dd71ec59e11c40317
HectorU <urdanetahector@johndeere.com>
2021-02-12 10:48:50 -0500

    Initial set up for caseybook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Link, Switch, Redirect } from 'react-router-dom';import logo from './logo.svg';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+        user:"Casey Harding",
+        username:"CRHarding",
+        image: "",
+        friendList: [] 
+      },
+      apiDataLoaded: false,
+      potentialFriends: []
+    }
+     
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  
+  };
 }
 
 export default App;

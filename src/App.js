f4bca906e1e86dd0d54b7ec67a7c25c815325ff8
HectorU <urdanetahector@johndeere.com>
2021-02-12 11:07:49 -0500

    caseybook Profile setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import './App.css';
 
-import { Route, Link, Switch, Redirect } from 'react-router-dom';import logo from './logo.svg';
+import Profile from './components/Profile';
+
+//import { Route, Link, Switch, Redirect } from 'react-router-dom';import logo from './logo.svg';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state= {
-        user:"Casey Harding",
+        user: {
+        name: "Casey Harding",
         username:"CRHarding",
-        image: "",
+        image: "https://pyxis.nymag.com/v1/imgs/d03/4b9/90a25fc9186862b0ce9c59b2529e792d32-StanLeevulturefinal.rhorizontal.w1100.jpg",
         friendList: [] 
       },
       apiDataLoaded: false,
       potentialFriends: []
-    }
+    }  
+  }
      
 
   render() {
     return (
       <div className="App">
+        <h1>CaseyBook</h1>
+        <Profile />
         
       </div>
     );

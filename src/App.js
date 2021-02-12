ca36f530ce414bf2ffba42249a6d95a03d4d15e7
HectorU <urdanetahector@johndeere.com>
2021-02-12 14:37:55 -0500

    loaded axios to query random user api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
+import axios from 'axios';
 
 import Profile from './components/Profile';
 import FriendsPage from './components/FriendsPage';
@@ -20,14 +21,31 @@ class App extends Component {
       potentialFriends: []
     }  
   }
-     
+  componentDidMount = async () => {  
+    const friendOne = await axios.get("https://randomuser.me/api/");
+    const friendTwo = await axios.get("https://randomuser.me/api/");
+    const friendThree = await axios.get("https://randomuser.me/api/");
+    const friendFour = await axios.get("https://randomuser.me/api/");
+    const friendFive = await axios.get("https://randomuser.me/api/");
+    
+    const friends = [
+      friendOne.data.results[0],
+      friendTwo.data.results[0],
+      friendThree.data.results[0],
+      friendFour.data.results[0],
+      friendFive.data.results[0]];
+    this.setState ({
+      potentialFriends: friends,
+      apiDataLoaded: true
+    })
+  }     
 
   render() {
     return (
       <div className="App">
         <h1>CaseyBook</h1>
         <Profile user={this.state.user} />
-        <FriendsPage />
+        <FriendsPage potentialFriends={this.state.potentialFriends}/>
         
       </div>
     );
